0668be80d65acfaef8926cc8ecc1a40f8ea5716b
Ghassen El Jezi <147505602+GhassenEl@users.noreply.github.com>
2024-02-27 19:35:01 +0100

    Add files via upload

--- src/Main.java
+++ src/Main.java
@@ -1,158 +1,491 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-import java.util.ArrayList;
-import java.util.List;
-
-public class Zoo {
-    private int capacity;
-    private List<String> animals;
-
-    public Zoo(int capacity) {
-        this.capacity = capacity;
-        this.animals = new ArrayList<>();
-    }
-
-    public void addAnimal(String animal) {
-        if (!isZooFull()) {
-            animals.add(animal);
-            System.out.println(animal + " has been added to the zoo!");
-        } else {
-            System.out.println("Sorry, the zoo is full");
-        }
-    }
-
-    public boolean isZooFull() {
-        return animals.size() >= capacity;
-    }
-
-    public static void main(String[] args) {
-        Zoo zoo = new Zoo(5); // Example capacity of 5
-        zoo.addAnimal("Lion");
-        zoo.addAnimal("Tiger");
-        zoo.addAnimal("Elephant");
-        zoo.addAnimal("Giraffe");
-        zoo.addAnimal("Monkey");
-        zoo.addAnimal("Zebra"); // This won't be added as the zoo is full
-    }
-}
+// Classe parente Animal
 public class Animal {
-    private String name;
-    private int age;
+    private String habitat; // L'habitat de l'animal
 
-    public Animal(String name, int age) {
-        this.name = name;
-        setAge(age);
+    // Constructeur
+    public Animal(String habitat) {
+        this.habitat = habitat;
     }
 
-    public String getName() {
-        return name;
+    // Getter pour l'habitat
+    public String getHabitat() {
+        return habitat;
     }
 
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        if (age >= 0) {
-            this.age = age;
-        } else {
-            throw new IllegalArgumentException("Age cannot be negative");
-        }
+    // Setter pour l'habitat
+    public void setHabitat(String habitat) {
+        this.habitat = habitat;
     }
 }
 
-public class Zoo {
-    private String name;
-    private int capacity;
-    private List<Animal> animals;
-
-    public Zoo(String name, int capacity) {
-        setName(name);
-        this.capacity = capacity;
-        this.animals = new ArrayList<>();
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        if (name != null && !name.isEmpty()) {
-            this.name = name;
-        } else {
-            throw new IllegalArgumentException("Zoo name cannot be empty");
-        }
-    }
-
-    // Other methods for managing animals in the zoo
-}
-package tn.esprit.gestionzoo.entities;
-import java.util.ArrayList;
-import java.util.List;
-public class Animal {
-    private String name;
-    private int age;
-
-    public Animal(String name, int age) {
-        this.name = name;
-        setAge(age);
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        if (age >= 0) {
-            this.age = age;
-        } else {
-            throw new IllegalArgumentException("Age cannot be negative");
-        }
+// Classe Animal aquatique
+public class Aquatic extends Animal {
+    // Constructeur
+    public Aquatic(String habitat) {
+        super(habitat);
     }
 }
-package tn.esprit.gestionzoo.entities;
-import java.util.ArrayList;
-import java.util.List;
 
-public class Zoo {
-    private String name;
-    private int capacity;
-    private List<Animal> animals;
+// Classe Animal terrestre
+public class Terrestrial extends Animal {
+    private int nbrLegs; // Nombre de pattes
 
-    public Zoo(String name, int capacity) {
-        setName(name);
-        this.capacity = capacity;
-        this.animals = new ArrayList<>();
+    // Constructeur
+    public Terrestrial(String habitat, int nbrLegs) {
+        super(habitat);
+        this.nbrLegs = nbrLegs;
     }
 
-    public String getName() {
-        return name;
+    // Getter pour le nombre de pattes
+    public int getNbrLegs() {
+        return nbrLegs;
     }
 
-    public void setName(String name) {
-        if (name != null && !name.isEmpty()) {
-            this.name = name;
-        } else {
-            throw new IllegalArgumentException("Zoo name cannot be empty");
-        }
+    // Setter pour le nombre de pattes
+    public void setNbrLegs(int nbrLegs) {
+        this.nbrLegs = nbrLegs;
     }
-
-    // Other methods for managing animals in the zoo
 }
-package tn.esprit.gestionzoo.main;
-import tn.esprit.gestionzoo.entities.Animal;
-import tn.esprit.gestionzoo.entities.Zoo;
 
+// Classe Dauphin
+public class Dolphin extends Aquatic {
+    private float swimmingSpeed; // Vitesse de nage
+
+    // Constructeur
+    public Dolphin(String habitat, float swimmingSpeed) {
+        super(habitat);
+        this.swimmingSpeed = swimmingSpeed;
+    }
+
+    // Getter pour la vitesse de nage
+    public float getSwimmingSpeed() {
+        return swimmingSpeed;
+    }
+
+    // Setter pour la vitesse de nage
+    public void setSwimmingSpeed(float swimmingSpeed) {
+        this.swimmingSpeed = swimmingSpeed;
+    }
+}
+
+// Classe Pingouin
+public class Penguin extends Aquatic {
+    private float swimmingDepth; // Profondeur de nage
+
+    // Constructeur
+    public Penguin(String habitat, float swimmingDepth) {
+        super(habitat);
+        this.swimmingDepth = swimmingDepth;
+    }
+
+    // Getter pour la profondeur de nage
+    public float getSwimmingDepth() {
+        return swimmingDepth;
+    }
+
+    // Setter pour la profondeur de nage
+    public void setSwimmingDepth(float swimmingDepth) {
+        this.swimmingDepth = swimmingDepth;
+    }
+}
 public class Main {
     public static void main(String[] args) {
-        // Example usage
-        Zoo zoo = new Zoo("MyZoo", 10);
-        Animal lion = new Animal("Lion", 5);
-        zoo.addAnimal(lion);
-        // Other operations
+        // Création d'une instance de chaque classe avec les constructeurs par défaut
+
+        // Animal aquatique
+        Aquatic aquaticAnimal = new Aquatic("Océan");
+
+        // Animal terrestre
+        Terrestrial terrestrialAnimal = new Terrestrial("Forêt", 4);
+
+        // Dauphin
+        Dolphin dolphin = new Dolphin("Océan", 20.5f); // Vitesse de nage = 20.5
+
+        // Pingouin
+        Penguin penguin = new Penguin("Océan", 30.0f); // Profondeur de nage = 30.0
+    }
+}
+// Classe parente Animal
+public class Animal {
+    private String habitat;
+
+    // Constructeur paramétré
+    public Animal(String habitat) {
+        this.habitat = habitat;
+    }
+
+    // Getter et Setter pour l'habitat
+    public String getHabitat() {
+        return habitat;
+    }
+
+    public void setHabitat(String habitat) {
+        this.habitat = habitat;
+    }
+}
+
+// Classe Animal aquatique
+public class Aquatic extends Animal {
+    // Constructeur paramétré
+    public Aquatic(String habitat) {
+        super(habitat);
+    }
+}
+
+// Classe Animal terrestre
+public class Terrestrial extends Animal {
+    private int nbrLegs;
+
+    // Constructeur paramétré
+    public Terrestrial(String habitat, int nbrLegs) {
+        super(habitat);
+        this.nbrLegs = nbrLegs;
+    }
+
+    // Getter et Setter pour le nombre de pattes
+    public int getNbrLegs() {
+        return nbrLegs;
+    }
+
+    public void setNbrLegs(int nbrLegs) {
+        this.nbrLegs = nbrLegs;
+    }
+}
+
+// Classe Dauphin
+public class Dolphin extends Aquatic {
+    private float swimmingSpeed;
+
+    // Constructeur paramétré
+    public Dolphin(String habitat, float swimmingSpeed) {
+        super(habitat);
+        this.swimmingSpeed = swimmingSpeed;
+    }
+
+    // Getter et Setter pour la vitesse de nage
+    public float getSwimmingSpeed() {
+        return swimmingSpeed;
+    }
+
+    public void setSwimmingSpeed(float swimmingSpeed) {
+        this.swimmingSpeed = swimmingSpeed;
+    }
+}
+
+// Classe Pingouin
+public class Penguin extends Aquatic {
+    private float swimmingDepth;
+
+    // Constructeur paramétré
+    public Penguin(String habitat, float swimmingDepth) {
+        super(habitat);
+        this.swimmingDepth = swimmingDepth;
+    }
+
+    // Getter et Setter pour la profondeur de nage
+    public float getSwimmingDepth() {
+        return swimmingDepth;
+    }
+
+    public void setSwimmingDepth(float swimmingDepth) {
+        this.swimmingDepth = swimmingDepth;
+    }
+}
+
+// Classe Main pour tester
+public class Main {
+    public static void main(String[] args) {
+        // Création d'instances avec les constructeurs paramétrés
+
+        // Animal aquatique
+        Aquatic aquaticAnimal = new Aquatic("Océan");
+
+        // Animal terrestre
+        Terrestrial terrestrialAnimal = new Terrestrial("Forêt", 4);
+
+        // Dauphin
+        Dolphin dolphin = new Dolphin("Océan", 20.5f); // Vitesse de nage = 20.5
+
+        // Pingouin
+        Penguin penguin = new Penguin("Océan", 30.0f); // Profondeur de nage = 30.0
+    }
+}
+// Classe parente Animal
+public class Animal {
+    private String habitat;
+
+    // Constructeur paramétré
+    public Animal(String habitat) {
+        this.habitat = habitat;
+    }
+
+    // Getter et Setter pour l'habitat
+    public String getHabitat() {
+        return habitat;
+    }
+
+    public void setHabitat(String habitat) {
+        this.habitat = habitat;
+    }
+}
+
+// Classe Animal aquatique
+public class Aquatic extends Animal {
+    // Constructeur paramétré
+    public Aquatic(String habitat) {
+        super(habitat);
+    }
+}
+
+// Classe Animal terrestre
+public class Terrestrial extends Animal {
+    private int nbrLegs;
+
+    // Constructeur paramétré
+    public Terrestrial(String habitat, int nbrLegs) {
+        super(habitat);
+        this.nbrLegs = nbrLegs;
+    }
+
+    // Getter et Setter pour le nombre de pattes
+    public int getNbrLegs() {
+        return nbrLegs;
+    }
+
+    public void setNbrLegs(int nbrLegs) {
+        this.nbrLegs = nbrLegs;
+    }
+}
+
+// Classe Dauphin
+public class Dolphin extends Aquatic {
+    private float swimmingSpeed;
+
+    // Constructeur paramétré
+    public Dolphin(String habitat, float swimmingSpeed) {
+        super(habitat);
+        this.swimmingSpeed = swimmingSpeed;
+    }
+
+    // Getter et Setter pour la vitesse de nage
+    public float getSwimmingSpeed() {
+        return swimmingSpeed;
+    }
+
+    public void setSwimmingSpeed(float swimmingSpeed) {
+        this.swimmingSpeed = swimmingSpeed;
+    }
+}
+
+// Classe Pingouin
+public class Penguin extends Aquatic {
+    private float swimmingDepth;
+
+    // Constructeur paramétré
+    public Penguin(String habitat, float swimmingDepth) {
+        super(habitat);
+        this.swimmingDepth = swimmingDepth;
+    }
+
+    // Getter et Setter pour la profondeur de nage
+    public float getSwimmingDepth() {
+        return swimmingDepth;
+    }
+
+    public void setSwimmingDepth(float swimmingDepth) {
+        this.swimmingDepth = swimmingDepth;
+    }
+}
+
+// Classe Main pour tester
+public class Main {
+    public static void main(String[] args) {
+        // Création d'instances avec les constructeurs paramétrés
+
+        // Animal aquatique
+        Aquatic aquaticAnimal = new Aquatic("Océan");
+
+        // Animal terrestre
+        Terrestrial terrestrialAnimal = new Terrestrial("Forêt", 4);
+
+        // Dauphin
+        Dolphin dolphin = new Dolphin("Océan", 20.5f); // Vitesse de nage = 20.5
+
+        // Pingouin
+        Penguin penguin = new Penguin("Océan", 30.0f); // Profondeur de nage = 30.0
+    }
+}
+// Classe Animal
+public class Animal {
+    private String habitat;
+
+    public Animal(String habitat) {
+        this.habitat = habitat;
+    }
+
+    public String getHabitat() {
+        return habitat;
+    }
+
+    public void setHabitat(String habitat) {
+        this.habitat = habitat;
+    }
+
+    @Override
+    public String toString() {
+        return "Habitat: " + habitat;
+    }
+}
+
+// Classe Aquatic
+public class Aquatic extends Animal {
+    public Aquatic(String habitat) {
+        super(habitat);
+    }
+}
+
+// Classe Terrestrial
+public class Terrestrial extends Animal {
+    private int nbrLegs;
+
+    public Terrestrial(String habitat, int nbrLegs) {
+        super(habitat);
+        this.nbrLegs = nbrLegs;
+    }
+
+    public int getNbrLegs() {
+        return nbrLegs;
+    }
+
+    public void setNbrLegs(int nbrLegs) {
+        this.nbrLegs = nbrLegs;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + ", Number of Legs: " + nbrLegs;
+    }
+}
+
+// Classe Dolphin
+public class Dolphin extends Aquatic {
+    private float swimmingSpeed;
+
+    public Dolphin(String habitat, float swimmingSpeed) {
+        super(habitat);
+        this.swimmingSpeed = swimmingSpeed;
+    }
+
+    public float getSwimmingSpeed() {
+        return swimmingSpeed;
+    }
+
+    public void setSwimmingSpeed(float swimmingSpeed) {
+        this.swimmingSpeed = swimmingSpeed;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + ", Swimming Speed: " + swimmingSpeed;
+    }
+}
+
+// Classe Penguin
+public class Penguin extends Aquatic {
+    private float swimmingDepth;
+
+    public Penguin(String habitat, float swimmingDepth) {
+        super(habitat);
+        this.swimmingDepth = swimmingDepth;
+    }
+
+    public float getSwimmingDepth() {
+        return swimmingDepth;
+    }
+
+    public void setSwimmingDepth(float swimmingDepth) {
+        this.swimmingDepth = swimmingDepth;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + ", Swimming Depth: " + swimmingDepth;
+    }
+}
+
+// Classe Main pour tester
+public class Main {
+    public static void main(String[] args) {
+        // Création d'instances avec les constructeurs paramétrés
+
+        // Animal aquatique
+        Aquatic aquaticAnimal = new Aquatic("Océan");
+        System.out.println("Aquatic Animal: " + aquaticAnimal);
+
+        // Animal terrestre
+        Terrestrial terrestrialAnimal = new Terrestrial("Forêt", 4);
+        System.out.println("Terrestrial Animal: " + terrestrialAnimal);
+
+        // Dauphin
+        Dolphin dolphin = new Dolphin("Océan", 20.5f); // Vitesse de nage = 20.5
+        System.out.println("Dolphin: " + dolphin);
+
+        // Pingouin
+        Penguin penguin = new Penguin("Océan", 30.0f); // Profondeur de nage = 30.0
+        System.out.println("Penguin: " + penguin);
+    }
+}
+// Classe Aquatic
+public class Aquatic extends Animal {
+    public Aquatic(String habitat) {
+        super(habitat);
+    }
+
+    public void swim() {
+        System.out.println("This aquatic animal is swimming.");
+    }
+}
+
+// Classe Dolphin
+public class Dolphin extends Aquatic {
+    private float swimmingSpeed;
+
+    public Dolphin(String habitat, float swimmingSpeed) {
+        super(habitat);
+        this.swimmingSpeed = swimmingSpeed;
+    }
+
+    @Override
+    public void swim() {
+        System.out.println("This dolphin is swimming.");
+    }
+}
+
+// Classe Penguin
+public class Penguin extends Aquatic {
+    private float swimmingDepth;
+
+    public Penguin(String habitat, float swimmingDepth) {
+        super(habitat);
+        this.swimmingDepth = swimmingDepth;
+    }
+}
+
+// Classe Main pour tester
+public class Main {
+    public static void main(String[] args) {
+        // Création d'instances et appel de la méthode swim()
+
+        // Aquatic
+        Aquatic aquaticAnimal = new Aquatic("Ocean");
+        aquaticAnimal.swim();
+
+        // Dolphin
+        Dolphin dolphin = new Dolphin("Ocean", 20.5f);
+        dolphin.swim();
+
+        // Penguin
+        Penguin penguin = new Penguin("Ocean", 30.0f);
+        // penguin.swim(); // Impossible d'appeler swim() sur un objet de type Penguin
     }
 }
