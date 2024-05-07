ed3798faeec8a1ce8b9e035f1d3a65953a4e936a
Ghassen El Jezi <147505602+GhassenEl@users.noreply.github.com>
2024-05-07 18:56:22 +0200

    Add files via upload

---
+++ Gaston Java JDK/Ghassen Prosit12.c
@@ -0,0 +1,110 @@
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Predicate;
+import java.util.function.Function;
+import java.util.function.Supplier;
+import java.util.Comparator;
+import java.util.stream.Stream;
+
+public class Student {
+    private int id;
+    private String name;
+    private int age;
+
+    // Constructeurs
+    public Student() {
+    }
+
+    public Student(int id, String name, int age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+
+    // Getters & Setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    // Redéfinition de la méthode toString
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
+
+public interface Management {
+    void displayStudents(List<Student> students, Consumer<Student> con);
+    void displayStudentsByFilter(List<Student> students, Predicate<Student> pre, Consumer<Student> con);
+    String returnStudentsNames(List<Student> students, Function<Student, String> fun);
+    Student createStudent(Supplier<Student> sup);
+    List<Student> sortStudentsById(List<Student> students, Comparator<Student> com);
+    Stream<Student> convertToStream(List<Student> students);
+}
+
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Predicate;
+import java.util.function.Function;
+import java.util.function.Supplier;
+import java.util.Comparator;
+import java.util.stream.Stream;
+
+public class StudentManagement implements Management {
+    @Override
+    public void displayStudents(List<Student> students, Consumer<Student> con) {
+        students.forEach(con);
+    }
+
+    @Override
+    public void displayStudentsByFilter(List<Student> students, Predicate<Student> pre, Consumer<Student> con) {
+        students.stream().filter(pre).forEach(con);
+    }
+
+    @Override
+    public String returnStudentsNames(List<Student> students, Function<Student, String> fun) {
+        StringBuilder names = new StringBuilder();
+        students.forEach(student -> names.append(fun.apply(student)).append(", "));
+        return names.toString();
+    }
+
+    @Override
+    public Student createStudent(Supplier<Student> sup) {
+        return sup.get();
+    }
+
+    @Override
+    public List<Student> sortStudentsById(List<Student> students, Comparator<Student> com) {
+        students.sort(com);
+        return students;
+    }
+
+    @Override
+    public Stream<Student> convertToStream(List<Student> students) {
+        return students.stream();
+    }
+}
